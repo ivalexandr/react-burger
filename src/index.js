c33a6d3633c80b7ab21a3f5666a23202c438d6aa
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2021-05-30 20:11:08 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './components/App/App'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
